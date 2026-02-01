a31ae0764fcffd3d399abef5730190bcc38e9c36
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2026-02-01 01:45:47 +0000

    chore: sync

--- NEUROSYM.scm
+++ NEUROSYM.scm
@@ -1,4 +1,4 @@
-;; SPDX-License-Identifier: PMPL-1.0
+;; SPDX-License-Identifier: PMPL-1.0-or-later
 ;; NEUROSYM.scm - Symbolic Semantics and Proof Obligations
 ;; protocol-squisher
 
--- PLAYBOOK.scm
+++ PLAYBOOK.scm
@@ -1,4 +1,4 @@
-;; SPDX-License-Identifier: PMPL-1.0
+;; SPDX-License-Identifier: PMPL-1.0-or-later
 ;; PLAYBOOK.scm - Executable Plans and Procedures
 ;; protocol-squisher
 
